c42ae54b6e0d812ccb18bad402f943913a917bf4
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2025-07-13 14:47:26 -0300

    Update TaskCard.tsx

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -9,7 +9,7 @@ export default function TaskCard({ task }: TaskCardProps) {
     return (
         <div className="flex gap-2 items-center">
             <img src={task.completed ? '/checkbox_signed.svg' : '/checkbox.svg'} alt="" />
-            <p className={`${task.completed ? 'opacity-25' : 'opacity-100'}`}>
+            <p className={`${task.completed ? 'opacity-50' : 'opacity-100'}`}>
                 {task.completed ? (
                     <s> {task.description} </s>
                 ) : (
