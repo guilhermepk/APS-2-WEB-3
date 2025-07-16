a3a5e07f461be978951ba16ac1e94b7dad52b298
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2025-07-16 01:12:21 -0300

    Update find-all-projects.ts

--- frontend/src/apis/backend/projects/find-all-projects.ts
+++ frontend/src/apis/backend/projects/find-all-projects.ts
@@ -1,7 +1,23 @@
+import iziToast from 'izitoast';
 import { backendApi } from '../axios-backend-api';
 import { FindAllProjectsResponseDto } from './models/dtos/find-all-projects-response.dto';
 
 export default async function findAllProjects(): Promise<FindAllProjectsResponseDto> {
-    return backendApi.get<FindAllProjectsResponseDto>('/projects')
-        .then(response => response.data);
+    return backendApi.get<FindAllProjectsResponseDto>(
+        '/projects',
+        {
+            customErrorInterceptor: (error: any) => {
+                if (error.response?.status === 404) {
+                    return Promise.resolve({ data: [] });
+                } else {
+                    iziToast.error({
+                        title: 'Erro',
+                        message: error.response?.data?.message || error.message || 'Erro desconhecido',
+                        position: 'topRight',
+                    });
+                }
+            }
+        }
+    ).then(response => response.data);
 }
+
